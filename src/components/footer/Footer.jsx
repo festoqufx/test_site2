aba25e27fb46bb62924ee34bc8013cb165d5d11d
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-21 19:19:54 +0800

    v5

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { BsLinkedin } from 'react-icons/bs';
-import { FaGithub } from 'react-icons/fa';
+import React, { useEffect } from "react";
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaTwitterSquare } from 'react-icons/fa';
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/ai';
 import './footer.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Footer = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+     });
+  }, []);
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <footer>
-      <a href="#home" className="footer__logo">Rasif Tagizade</a>
+    <footer data-aos="zoom-out">
+      <h1 style={{color: "black"}}>FOLLOW ME AROUND THE WEB</h1>
+      <p>&nbsp;</p>
+      
+      <div className="footer__socials">
+        <a href="#home" target="_blank" rel="noreferrer" ><FaFacebookSquare /></a>
+        <a href="#home" target="_blank" rel="noreferrer" ><FaLinkedin /></a>
+        <a href="#home" target="_blank" rel="noreferrer" ><FaInstagramSquare /></a>
+        <a href="#home" target="_blank" rel="noreferrer" ><FaTwitterSquare /></a>
+        <a href="#home" target="_blank" rel="noreferrer" ><FaGithubSquare /></a>
+      </div>
+
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
@@ -15,15 +41,15 @@ const Footer = () => {
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <div className="footer__socials">
-      <a href="https://www.linkedin.com/in/rasif-tagizada/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-      <a href="https://github.com/Rasif-Taghizada" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      </div>
       <div className="footer__copyright">
-        <small>&copy; ET 2022. All rights reserved.</small>
+        <small>&copy; ET {year}. |  FERDINAND&nbsp;ESTOQUE   |   All&nbsp;Rights&nbsp;Reserved.</small>
       </div>
+      <p>&nbsp;</p>
+     
+      <p>Made with <AiOutlineHeart/> by me.</p>
+      <p>&nbsp;</p>
+
     </footer>
   )
 }
-
 export default Footer
