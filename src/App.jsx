0fc523a1fcb6657580a36a1d94044fb6edc6c0ef
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-21 18:50:38 +0800

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from './components/experience/Experience';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Intro from './components/intro/Intro';
-import Portfolio from './components/portfolio/Portfolio';
+// import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
 import Topbar from './components/topbar/Topbar';
 
@@ -16,7 +16,7 @@ const App = () => {
       <Topbar />
       <Intro />
       <Experience />
-      <Portfolio />
+      {/* <Portfolio /> */}
       <Testimonials />
       <Contact />
       <Footer />
