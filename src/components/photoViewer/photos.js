903d4b34c8911e29869fedcb759cba35ca0655b5
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-22 20:48:03 +0800

    images

--- src/components/photoViewer/photos.js
+++ src/components/photoViewer/photos.js
@@ -1,91 +1,91 @@
 export const photos = [
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-7.jpg",
+      src: "/superbox/superbox2-full-7.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-8.jpg",
+      src: "/superbox/superbox2-full-8.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-9.jpg",
+      src: "/superbox/superbox2-full-9.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-10.jpg",
+      src: "/superbox/superbox2-full-10.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-11.jpg",
+      src: "/superbox/superbox2-full-11.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-12.jpg",
+      src: "/superbox/superbox2-full-12.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-13.jpg",
+      src: "/superbox/superbox2-full-13.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-14.jpg",
+      src: "/superbox/superbox2-full-14.jpg",
       width: 4,
       height: 3
     },
     {
-      src: "https://ferdinandestoque.com/img/superbox/superbox2-full-15.jpg",
+      src: "/superbox/superbox2-full-15.jpg",
       width: 4,
       height: 3
     },
     {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-16.jpg",
+        src: "/superbox/superbox2-full-16.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-17.jpg",
+        src: "/superbox/superbox2-full-17.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-18.jpg",
+        src: "/superbox/superbox2-full-18.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-19.jpg",
+        src: "/superbox/superbox2-full-19.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-20.jpg",
+        src: "/superbox/superbox2-full-20.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-21.jpg",
+        src: "/superbox/superbox2-full-21.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-22.jpg",
+        src: "/superbox/superbox2-full-22.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-23.jpg",
+        src: "/superbox/superbox2-full-23.jpg",
         width: 4,
         height: 3
       },
       {
-        src: "https://ferdinandestoque.com/img/superbox/superbox2-full-24.jpg",
+        src: "/superbox/superbox2-full-24.jpg",
         width: 4,
         height: 3
       }
