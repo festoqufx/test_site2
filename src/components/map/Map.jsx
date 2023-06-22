d80395549a2e2b1cd45d8a3967b11e87b5dac323
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-22 21:20:42 +0800

    asads

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -21,7 +21,7 @@ const Map = () => {
     
       <div className="mapouter">
       <div className="gmap_canvas">
-      <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=2048&amp;height=800&amp;hl=en&amp;q=TEA garden imus&amp;t=p&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+      <iframe title="map" class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=2048&amp;height=800&amp;hl=en&amp;q=TEA garden imus&amp;t=p&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
 
     </div>
