141db6e3d615bfe15ccde70d8fa564b968016c8f
Ferdinand Estoque <festoqufx@gmail.com>
2023-06-24 13:14:42 +0800

    v4

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -40,8 +40,8 @@ const Contact = () => {
           <article className="contact__option">
             <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
-            <h5>rasiftagizade11@gmail.com</h5>
-            <a href="mailto:mekomerikomg@gmail.com">Send a message</a>
+            <h5>ferdinand.estoque@yahoo.com</h5>
+            <a href="mailto:ferdinand.estoque@yahoo.com">Send a message</a>
           </article>
         </div>
         <form ref={formRef} onSubmit={handleSubmit}>
